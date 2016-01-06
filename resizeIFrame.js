ddc6ae0c81ee2181ae0e470404e9f08082d7c72b
BradlyTamaki <bradlytamaki@gmail.com>
2016-01-05 17:32:30 -0800

    Update resizeIFrame.js

--- resizeIFrame.js
+++ resizeIFrame.js
@@ -25,3 +25,13 @@ function resizeIFrame(height, width) {
             + "resize(" + width + "," + height + ")</Message>";
     parent.postMessage(message, '*');
 }
+function getQueryStringParameter(param) {
+    var params = document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == param) {
+            return singleParam[1];
+        }
+    }
+}
